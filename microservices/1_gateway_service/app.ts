f48f27123472a08c65c55c6398672809e3145b0d
Roy P A <roy@nintrivalabs.com>
2025-01-22 12:06:02 +0530

    fix: some changes

---
+++ microservices/1_gateway_service/app.ts
@@ -0,0 +1,15 @@
+import express, {Express } from 'express';
+
+
+
+class Application{
+  public initialize(): void {
+    const app: Express = express();
+
+
+  }
+
+}
+
+const application: Application = new Application();
+application.initialize();
