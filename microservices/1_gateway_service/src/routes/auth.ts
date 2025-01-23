a0c8d8a4663d6ad66418fed3eaf5adde992f354c
Roy P A <roy@nintrivalabs.com>
2025-01-23 10:58:01 +0530

    fix: some changes

--- microservices/1_gateway_service/src/routes/auth.ts
+++ microservices/1_gateway_service/src/routes/auth.ts
@@ -1,19 +1,30 @@
 import express, { Router } from 'express';
 import { SignIn } from '@gateway/controllers/auth/signin';
+import { SignUp } from '@gateway/controllers/auth/signup';
+import { Signout } from '@gateway/controllers/auth/signout';
+import { VerifyEmail } from '@gateway/controllers/auth/verify-email';
+import { VerifyOTP } from '@gateway/controllers/auth/verify-otp';
+import { Password } from '@gateway/controllers/auth/password';
 
 
 class AuthRoutes {
   private router: Router;
-
+  
   constructor() {
     this.router = express.Router();
   }
 
   public routes(): Router {
+    this.router.post('/auth/signup', SignUp.prototype.create);
     this.router.post('/auth/signin', SignIn.prototype.read);
+    this.router.post('/auth/signout', Signout.prototype.update);
+    this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
+    this.router.put('/auth/verify-otp/:otp', VerifyOTP.prototype.update);
+    this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
+    this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
+    this.router.put('/auth/change-password', Password.prototype.changePassword);
     return this.router;
   }
-
 }
 
 export const authRoutes: AuthRoutes = new AuthRoutes();
--- microservices/1_gateway_service/src/services/api/auth.service.ts
+++ microservices/1_gateway_service/src/services/api/auth.service.ts
@@ -13,9 +13,49 @@ class AuthService {
     axiosAuthInstance = this.axiosService.axios;
   }
 
+  async getRefreshToken(username: string): Promise<AxiosResponse> {
+    const response: AxiosResponse = await axiosAuthInstance.get(`/refresh-token/${username}`);
+    return response;
+  }
+
+  async changePassword(currentPassword: string, newPassword: string): Promise<AxiosResponse> {
+    const response: AxiosResponse = await axiosAuthInstance.put('/change-password', { currentPassword, newPassword });
+    return response;
+  }
+
+  async verifyEmail(token: string): Promise<AxiosResponse> {
+    const response: AxiosResponse = await axiosAuthInstance.put('/verify-email', { token });
+    return response;
+  }
+
+  async verifyOTP(otp: string, body: { browserName: string, deviceType: string }): Promise<AxiosResponse> {
+    const response: AxiosResponse = await axiosAuthInstance.put(`/verify-otp/${otp}`, body);
+    return response;
+  }
+
+  async resendEmail(data: { userId: number, email: string }): Promise<AxiosResponse> {
+    const response: AxiosResponse = await axiosAuthInstance.post('/resend-email', data);
+    return response;
+  }
+
+  async signUp(body: any): Promise<AxiosResponse> {
+    const response: AxiosResponse = await this.axiosService.axios.post('/signup', body);
+    return response;
+  }
+
 
   async signIn(body: any): Promise<AxiosResponse> {
     const response: AxiosResponse = await this.axiosService.axios.post('/signin', body)
+    return response;
+  }
+
+  async forgotPassword(email: string): Promise<AxiosResponse> {
+    const response: AxiosResponse = await this.axiosService.axios.put('/forgot-password', { email });
+    return response;
+  }
+
+  async resetPassword(token: string, password: string, confirmPassword: string): Promise<AxiosResponse> {
+    const response: AxiosResponse = await this.axiosService.axios.put(`/reset-password/${token}`, { password, confirmPassword });
     return response;
   }
 
