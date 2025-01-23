5b34ff0c1bc0a91b371b20cf4689b212345dc47c
Roy P A <roy@nintrivalabs.com>
2025-01-23 10:37:08 +0530

    fix: some changes

--- microservices/1_gateway_service/src/controllers/auth/signin.ts
+++ microservices/1_gateway_service/src/controllers/auth/signin.ts
@@ -5,10 +5,13 @@ import { authService } from '@gateway/services/api/auth.service';
 
 export class SignIn {
   public async read(req: Request, res: Response): Promise<void>{
-    
+
     const response: AxiosResponse = await authService.signIn(req.body);
-    const {message } = response.data;
-    res.status(StatusCodes.OK).json({message});
+    //const {message } = response.data;
+    const {test} = response.data;
+
+    //res.status(StatusCodes.OK).json({message});
+    res.status(StatusCodes.OK).json({test});
   }
 
 }
--- microservices/2_auth_service/src/controllers/signin.ts
+++ microservices/2_auth_service/src/controllers/signin.ts
@@ -3,6 +3,5 @@ import { StatusCodes } from 'http-status-codes';
 
 
 export async function read(req: Request, res: Response): Promise<void> {
-  console.log("pinupinu");
-  res.status(StatusCodes.OK).json();
+  res.status(StatusCodes.OK).json({test:"hello"});
 }
