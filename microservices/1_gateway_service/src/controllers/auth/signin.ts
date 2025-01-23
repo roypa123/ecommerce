89f82a8245c2c306538fc75c06f080af1f39e37e
Roy P A <roy@nintrivalabs.com>
2025-01-23 17:12:01 +0530

    fix: some changes

--- microservices/1_gateway_service/src/controllers/auth/signin.ts
+++ microservices/1_gateway_service/src/controllers/auth/signin.ts
@@ -1,16 +1,44 @@
 import { AxiosResponse } from 'axios';
 import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import { authService } from '@gateway/services/api/auth.service';
+import { StatusCodes } from 'http-status-codes';
 
 export class SignIn {
   public async read(req: Request, res: Response): Promise<void> {
 
-    const response: AxiosResponse = await authService.signIn(req.body);
+    try {
+      const response: AxiosResponse = await authService.signIn(req.body);
 
-    const { status, status_code, message, token, results } = response.data;
-    req.session = { jwt: token };
-    res.status(StatusCodes.OK).json({ status, status_code, message, results });
+      const { status, status_code, message, token, results } = response.data;
+      req.session = { jwt: token ?? "" };
+      res.status(status).json({
+        status: status,
+        status_code: status_code ?? "",
+        message: message ?? "",
+        results: results ?? "",
+      });
+
+    } catch (error: any) {
+
+      if (error.response) {
+        const { status, status_code, message, errors } = error.response.data;
+        res.status(status_code ?? StatusCodes.INTERNAL_SERVER_ERROR).json({ status, status_code, message, errors });
+      } else {
+
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          code: "500",
+          status: "INTERNAL SERVER ERROR",
+          message: "An unexpected error occurred",
+          errors: ["An unexpected error occurred"],
+        });
+      }
+
+    }
+
+
+
+
+
   }
 
 }
--- microservices/2_auth_service/src/controllers/signin.ts
+++ microservices/2_auth_service/src/controllers/signin.ts
@@ -3,12 +3,45 @@ import { Request, Response } from 'express';
 import { BadRequestError, IAuthDocument, IEmailMessageDetails, isEmail } from '@roy_p_a/ecommerce_shared';
 
 
+class ErrorVO {
+  status: number | undefined;
+  status_code: string | undefined;
+  message: String | undefined
+  errors: string[] | undefined;
+
+  constructor(status: number, status_code: string, message: String, errors: string | string[]) {
+    this.status = status;
+    this.status_code = status_code;
+    this.message = message;
+    this.errors = Array.isArray(errors) ? errors : [errors];
+  }
+}
+
+
 export async function read(req: Request, res: Response): Promise<void> {
   const userData = req.body;
-  console.log("dddd");
+
+  console.log(userData.email);
+  console.log(userData.password);
+  console.log(userData.role);
+
+  if (!userData.email && !userData.password && !userData.role) {
+    console.log("manu")
+    const validationError = new ErrorVO(
+      400,
+      "BAD REQUEST",
+      "Missing required fields",
+      "Missing required fields"
+    );
+    res.status(400).json(validationError);
+    return;
+
+  }
+
+  console.log("pinu")
 
   res.status(200).json({
-    status: userData.name,
+    status: 200,
     status_code: "",
     message: "",
     token: "",
